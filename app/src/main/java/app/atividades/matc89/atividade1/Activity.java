1e9501dcce9cb04bb19f4c4d42046ca36a118547
Arley Prates <arleyprates@gmail.com>
2016-04-09 10:48:11 -0300

    Done

---
+++ app/src/main/java/app/atividades/matc89/atividade1/Activity.java
@@ -0,0 +1,26 @@
+package app.atividades.matc89.atividade1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.widget.EditText;
+
+public class Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        String site = "";
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            site = extras.getString("Site");
+        }
+
+        WebView myWebView = (WebView) findViewById(R.id.web_view);
+        if (myWebView != null) {
+            myWebView.loadUrl(site);
+        }
+    }
+}
--- app/src/main/java/app/atividades/matc89/atividade1/MainActivity.java
+++ app/src/main/java/app/atividades/matc89/atividade1/MainActivity.java
@@ -2,6 +2,10 @@ package app.atividades.matc89.atividade1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.goSite);
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    Intent i = new Intent("app.atividades.matc89.atividade1.ACTIVITY");
+                    Bundle extras = new Bundle();
+                    EditText site = (EditText) findViewById(R.id.addressSite);
+                    extras.putString("Site", site.getText().toString());
+                    i.putExtras(extras);
+                    startActivityForResult(i, 1);
+                }
+            });
+        }
     }
 }
